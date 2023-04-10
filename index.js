154ef10a5040e95d013d9146af2fde95e46a6972
AzharTakoy <azharahmedtakoy4444@gmail.com>
2023-04-11 00:03:20 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,16 +1,58 @@
 function isPalindrome(word) {
+  //reverse the input string
+  let spliting=word.split('').reverse().join('')
+ // if the reversed string is the same as the input
+  if(spliting===word){
+//return true
+    return true
+  }
+  // else return false
+  else{
+      return false
+  }
+    
+    
   // Write your algorithm here
 }
 
-/* 
-  Add your pseudocode here
-*/
-
 /*
+create an array from the input string
+reverse the array
+create a string from the reversed array
+return the reversed string
   Add written explanation of your solution here
 */
 
 // You can run `node index.js` to view these console logs
+if (require.main === module) {
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("robot"));
+}
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
